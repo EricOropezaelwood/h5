df631bb5dabef93d27f33546cbe7cfc42092fdd1
Eric <eric.oropezaelwood@gmail.com>
2018-08-31 09:44:07 -0600

    standard deviation across columns added

--- h5Manip.py
+++ h5Manip.py
@@ -1,3 +1,9 @@
+'''
+Author: Eric Oropezaelwood
+Date: 31 August 2018
+Purpose: Take in an hdf5 file and run statistical calculations across
+the columns/frames.
+'''
 import h5py
 import numpy as np
 from scipy import stats
@@ -7,24 +13,48 @@ file = h5py.File('BEADS_NK_Dubai_1km_CFLOS_x060.h5', 'r')
 
 # display the keys ('GeoLocation Data', 'CalRawData', etc...)
 print(list(file.keys()))
+print("")
 
 # store one of the keys into a dataset
 CalDset = file['CalRawData']
+
 # display the dimensions of that dataset
-print(CalDset.shape)
-print(CalDset.dtype)
+print("Shape of dataset:",CalDset.shape)
+print("Data type of dataset:", CalDset.dtype)
+print("")
 
 # store the 'CalRawData' into a numpy dataframe
 CalNp = file['CalRawData'][...]
-print(CalNp.shape)
+print("Shape of numpy array:", CalNp.shape)
+print("")
+print("Numpy array: ")
+# np.set_printoptions(suppress=True)
+print(CalNp)
+print("")
 
-# find the MAX of the numpy CalNp
-maxCal = np.amax(CalNp)
-print("max is:", maxCal)
+# find the STANDARD DEVIATION of CalNp
+stdCalNp = np.std(CalNp, axis = 0) # axis = 0 represents columns
+print("Standard Deviation across columns: ")
+print(stdCalNp)
+print("")
 
-# find the MEAN of the numpy CalNP
-meanCal = np.mean(CalNp)
-print("mean is:", meanCal)
+# find the MEAN of the numpy array CalNP
+meanCal = np.mean(CalNp, axis = 0) # axis = 0 represents columns
+print("Mean across columns is:")
+print(meanCal)
+print("")
 
-# display the summary stastistics of the numpy CalNP
-print(stats.describe(CalNp))
+# find the MAX of the numpy array CalNp
+maxCal = np.amax(CalNp, axis = 0) # axis = 0 represents columns
+print("Max across columns: ")
+print(maxCal)
+print("")
+
+# find the MIN of the numpy array CalNp
+minCal = np.amin(CalNp, axis = 0) # axis = 0 represents columns
+print("Min across columns: ")
+print(minCal)
+print("")
+
+# display the summary stastistics of the numpy array CalNP
+#print(stats.describe(CalNp))
