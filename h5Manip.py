10fb8589807830dd9f65cad9e8ab29e39b6026ca
Eric Oropezaelwood <eric.oropezaelwood@gmail.com>
2018-09-12 09:00:27 -0600

    Now deletes a dataset and creates a new one

--- h5Manip.py
+++ h5Manip.py
@@ -15,7 +15,7 @@ def datasetToNumpyArray(inputFile):
 	outputNp = inputFile['CalRawData'][...]
 	print("Shape of numpy array from dataset 'CalRawData':", outputNp.shape)
 	print("")
-	print("Numpy array: ")
+	# print("Numpy array: ")
 	# np.set_printoptions(suppress=True) # suppress scientific data
 	print(outputNp)
 	print("")
@@ -24,25 +24,25 @@ def datasetToNumpyArray(inputFile):
 # Take in an array and run STD, MEAN, MAX and MIN across columns
 def statisticsAcrossColumns(inputArray):
 
-	# find the STANDARD DEVIATION of CalNp
+	# find the STANDARD DEVIATION of CalNp (ignoring NaN values)
 	stdArray = np.nanstd(inputArray, axis = 0) # axis = 0 represents columns
 	print("Standard Deviation across columns: ")
 	print(stdArray)
 	print("")
 
-	# find the MEAN of the numpy array CalNP
+	# find the MEAN of the numpy array CalNP (ignoring NaN values)
 	meanArray = np.nanmean(inputArray, axis = 0) # axis = 0 represents columns
 	print("Mean across columns is:")
 	print(meanArray)
 	print("")
 
-	# find the MAX of the numpy array CalNp
+	# find the MAX of the numpy array CalNp (ignoring NaN values)
 	maxArray = np.nanmax(inputArray, axis = 0) # axis = 0 represents columns
 	print("Max across columns: ")
 	print(maxArray)
 	print("")
 
-	# find the MIN of the numpy array CalNp
+	# find the MIN of the numpy array CalNp (ignoring NaN values)
 	minArray = np.nanmin(inputArray, axis = 0) # axis = 0 represents columns
 	print("Min across columns: ")
 	print(minArray)
@@ -50,26 +50,50 @@ def statisticsAcrossColumns(inputArray):
 
 
 # read in the .h5 file
-h5File = h5py.File('BEADS_NK_Dubai_1km_CFLOS_x060.h5', 'r')
+h5File = h5py.File('CFLOS.h5', 'r+') #ONLY RUN ONCE PER FILE!!!
 
 # display the keys ('GeoLocation Data', 'CalRawData', etc...)
 print(list(h5File.keys()))
 print("")
+
 '''
 # store one of the keys into a dataset
-CalDset = h5File['CalRawData']
+FirstDset = h5File['CalRawData']
 
 # display the dimensions of that dataset
-print("Shape of dataset:",CalDset.shape)
-print("Data type of dataset:", CalDset.dtype)
+print("Shape of dataset:",FirstDset.shape)
+print("Data type of dataset:", FirstDset.dtype)
 print("")
 '''
 
 # convert one dataset to numpy array 
 CalNp = datasetToNumpyArray(h5File)
+print(CalNp)
+print("")
+
+'''
+# find the STANDARD DEVIATION of CalNp (ignoring NaN values)
+for i in range(10800):
+	stdArray2 = np.nanstd(CalNp[i], axis = 0)
+
+print(stdArray2)
+'''
+
+
+meanArray = np.nanmean(CalNp, axis = 0) # axis = 0 represents columns
+print("Mean across columns: ")
+print(meanArray)
+print("")
+
+
+
+
+# SECOND TRY
+del h5File['CalRawData']
+
+# NEED TO ALTER THE SHAPE OF THE ARRAY stdArray (implement own standard deviation function across 10800 stacks of images)
+dset = h5File.create_dataset('CalRawData', data=meanArray)
+
 
 # call the stastics function
-statisticsAcrossColumns(CalNp)
-
-# display the summary stastistics of the numpy array CalNP
-#print(stats.describe(CalNp))
+# statisticsAcrossColumns(CalNp)
